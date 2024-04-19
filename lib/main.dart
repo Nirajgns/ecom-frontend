9ebd660ff1cec99b1f92c2fd820c3fe5b4732c70
Niraj Ganesh <niraj@2400-1A00-BD20.ip6.wlink.com.np>
2024-04-19 22:38:36 +0545

    implement bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:ecom_app/core/routes.dart';
 import 'package:ecom_app/core/ui.dart';
 import 'package:ecom_app/logic/cubits/user_cubit/user_cubit.dart';
@@ -7,6 +9,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
   runApp(const EcomerceApp());
 }
 
@@ -26,5 +29,31 @@ class EcomerceApp extends StatelessWidget {
         initialRoute: LoginScreen.routeName,
       ),
     );
+  }
+}
+
+class MyBlocObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase bloc) {
+    log("created bloc: $bloc");
+    super.onCreate(bloc);
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    log("Change in $bloc: $change");
+    super.onChange(bloc, change);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    log("ChangeIn $bloc: $transition");
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    log("Closed $bloc");
+    super.onClose(bloc);
   }
 }
