a725d0cd1d8a630c515c1fd6540bbd2f60c30e04
Niraj Ganesh <niraj@2400-1A00-BD20.ip6.wlink.com.np>
2024-04-22 22:37:33 +0545

    navigate to home

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -2,6 +2,7 @@ import 'package:ecom_app/presentation/screens/auth/login_screen.dart';
 import 'package:ecom_app/presentation/screens/auth/providers/login_provider.dart';
 import 'package:ecom_app/presentation/screens/auth/providers/signup_provider.dart';
 import 'package:ecom_app/presentation/screens/auth/signup_screen.dart';
+import 'package:ecom_app/presentation/screens/home/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
@@ -21,6 +22,9 @@ class Routes {
             builder: (context) => ChangeNotifierProvider(
                 create: (context) => SignupProvider(context),
                 child: const SignupScreen()));
+
+      case HomeScreen.routeName:
+        return CupertinoPageRoute(builder: (context) => const HomeScreen());
 
       default:
         return null;
--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -1,12 +1,16 @@
 import 'package:ecom_app/core/ui.dart';
+import 'package:ecom_app/logic/cubits/user_cubit/user_cubit.dart';
+import 'package:ecom_app/logic/cubits/user_cubit/user_state.dart';
 import 'package:ecom_app/presentation/screens/auth/providers/login_provider.dart';
 import 'package:ecom_app/presentation/screens/auth/signup_screen.dart';
+import 'package:ecom_app/presentation/screens/home/home_screen.dart';
 import 'package:ecom_app/presentation/widgets/gap_widget.dart';
 import 'package:ecom_app/presentation/widgets/link_button.dart';
 import 'package:ecom_app/presentation/widgets/primary_button.dart';
 import 'package:ecom_app/presentation/widgets/primary_textfield.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -23,43 +27,50 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     final provider = Provider.of<LoginProvider>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        title: const Text("Ecommerce App"),
-      ),
-      body: SafeArea(
-        child: Form(
-          key: provider.formkey,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ListView(children: [
-              Text("Log In", style: TextStyles.heading2),
-              const GapWidget(
-                size: -10,
-              ),
-              (provider.error != "")
-                  ? Text(provider.error,
-                      style: const TextStyle(color: Colors.red))
-                  : const SizedBox(),
-              const GapWidget(size: 5),
-              PrimaryTextField(
-                labelText: "Email Address",
-                controller: provider.emailController,
-                hintText: "Enter your Email...",
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return "Email address is required!";
-                  }
-                  if (!EmailValidator.validate(value.trim())) {
-                    return "Invalid email address!";
-                  }
-                  return null;
-                },
-              ),
-              const GapWidget(),
-              PrimaryTextField(
+    return BlocListener<UserCubit, UserState>(
+      listener: (context, state) {
+        if (state is UserLoggedInState) {
+          Navigator.popUntil(context, (route) => route.isFirst);
+          // Navigator.pushNamedAndRemoveUntil(context, HomeScreen.routeName, (route) => false);
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0,
+          title: const Text("Ecommerce App"),
+        ),
+        body: SafeArea(
+          child: Form(
+            key: provider.formkey,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ListView(children: [
+                Text("Log In", style: TextStyles.heading2),
+                const GapWidget(
+                  size: -10,
+                ),
+                (provider.error != "")
+                    ? Text(provider.error,
+                        style: const TextStyle(color: Colors.red))
+                    : const SizedBox(),
+                const GapWidget(size: 5),
+                PrimaryTextField(
+                  labelText: "Email Address",
+                  controller: provider.emailController,
+                  hintText: "Enter your Email...",
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "Email address is required!";
+                    }
+                    if (!EmailValidator.validate(value.trim())) {
+                      return "Invalid email address!";
+                    }
+                    return null;
+                  },
+                ),
+                const GapWidget(),
+                PrimaryTextField(
                   labelText: "Password",
                   hintText: "Enter your Password...",
                   obscureText: true,
@@ -69,35 +80,37 @@ class _LoginScreenState extends State<LoginScreen> {
                       return "Password is required!";
                     }
                     return null;
-                  }),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  LinkButton(
-                    text: "Forgot password?",
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-              const GapWidget(),
-              PrimaryButton(
-                text: (provider.isLoading) ? "..." : "Log In",
-                onPressed: provider.logIn,
-              ),
-              const GapWidget(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Don't have an account?"),
-                  LinkButton(
-                    text: "Sign Up",
-                    onPressed: () {
-                      Navigator.pushNamed(context, SignupScreen.routeName);
-                    },
-                  ),
-                ],
-              ),
-            ]),
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    LinkButton(
+                      text: "Forgot password?",
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                const GapWidget(),
+                PrimaryButton(
+                  text: (provider.isLoading) ? "..." : "Log In",
+                  onPressed: provider.logIn,
+                ),
+                const GapWidget(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account?"),
+                    LinkButton(
+                      text: "Sign Up",
+                      onPressed: () {
+                        Navigator.pushNamed(context, SignupScreen.routeName);
+                      },
+                    ),
+                  ],
+                ),
+              ]),
+            ),
           ),
         ),
       ),
